62e56bb0f7268c4d49433c85cb491ed1e93ae7de
skbrown333 <skbrown333@gmail.com>
2019-01-07 11:52:20 -0600

    Updates

---
+++ precache-manifest.fd826b77a62026453668f9550a9f7897.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/steffan/static/media/resume.d41d8cd9.pdf"
+  },
+  {
+    "revision": "1324738ecdafb1937a0e",
+    "url": "/steffan/static/css/main.6dbd7fc1.chunk.css"
+  },
+  {
+    "revision": "adaef7d56889d8131c56",
+    "url": "/steffan/static/js/1.adaef7d5.chunk.js"
+  },
+  {
+    "revision": "b991d3774a711a162417",
+    "url": "/steffan/static/js/runtime~main.b991d377.js"
+  },
+  {
+    "revision": "aab12d67c9f582b009263e89e7077585",
+    "url": "/steffan/static/media/computer-red.aab12d67.jpg"
+  },
+  {
+    "revision": "1324738ecdafb1937a0e",
+    "url": "/steffan/static/js/main.1324738e.chunk.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/steffan/static/media/cover-letter.d41d8cd9.pdf"
+  },
+  {
+    "revision": "32ba610b6480cfff434eb9460a849554",
+    "url": "/steffan/static/media/develop.32ba610b.svg"
+  },
+  {
+    "revision": "e53b6199b23683c38e87dd2133cb7ed4",
+    "url": "/steffan/static/media/computer-blue.e53b6199.jpg"
+  },
+  {
+    "revision": "bf1e9a8a21c3804c7264438821d8f16d",
+    "url": "/steffan/static/media/desk.bf1e9a8a.jpg"
+  },
+  {
+    "revision": "31de82cee83727b357a72587017f727c",
+    "url": "/steffan/index.html"
+  }
+];
